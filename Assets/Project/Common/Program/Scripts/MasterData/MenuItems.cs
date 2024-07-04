881f7f521f5dd57fe640695f798c7e6e66b322fb
hata0 <haataa0100@gmail.com>
2024-07-04 15:48:00 +0900

    feat: コメントを追加

--- Assets/Project/Common/Program/Scripts/MasterData/MenuItems.cs
+++ Assets/Project/Common/Program/Scripts/MasterData/MenuItems.cs
@@ -11,6 +11,7 @@ namespace Common.MasterData
     {
         // dotnetにMasterMemory.GeneratorとMessagePack.Generatorをインストールして、
         // inputとoutputのパスが正しいか確認する必要がある
+        // 実行後、画面遷移を挟まないと反映されないので注意
         [MenuItem("Tools/MasterData/GenerateCode")]
         private static void GenerateCode()
         {
