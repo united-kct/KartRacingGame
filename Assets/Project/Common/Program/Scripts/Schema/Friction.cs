751770cdc20a703c7b127f697ffd660aa2ee1831
hata0 <haataa0100@gmail.com>
2024-07-04 15:19:41 +0900

    refactor: Frictionのコードを整理

--- Assets/Project/Common/Program/Scripts/Schema/Friction.cs
+++ Assets/Project/Common/Program/Scripts/Schema/Friction.cs
@@ -6,15 +6,15 @@ namespace Common.Schema
     [MemoryTable("friction"), MessagePackObject(true)]
     public sealed class Friction
     {
+        [PrimaryKey] public string Id { get; }
+        [SecondaryKey(0)] public string TagName { get; }
+        public float FrictionalAcceleration { get; }
+
         public Friction(string id, string tagName, float frictionalAcceleration)
         {
             Id = id;
             TagName = tagName;
             FrictionalAcceleration = frictionalAcceleration;
         }
-
-        [PrimaryKey] public string Id { get; }
-        [SecondaryKey(0)] public string TagName { get; }
-        public float FrictionalAcceleration { get; }
     }
 }
